2263624bf864b7ca5278eaef014a285057bdec0c
ramunettam <ramunettam@gmail.com>
2024-05-19 14:05:34 +0200

    terraform vpc

--- variables.tf
+++ variables.tf
@@ -3,11 +3,6 @@ variable "project_name" {
 
 }
 
-variable "environment" {
-  
-  default = "dev"
-}
-
 variable "comman_tags" {
     type=map
     default = {
@@ -21,3 +16,17 @@ variable "public_cidrs" {
     type = list
     default = ["10.0.1.0/24","10.0.2.0/24"]
 }
+
+##private  subnet cidrs
+
+variable "private_cidrs"{
+    type=list
+
+    default = [ "10.0.11.0/24","10.0.12.0/24"]
+    }
+variable "database_cidrs" {
+    type = list
+    default = [ "10.0.21.0/24","10.0.22.0/24" ]
+  
+}    
+
--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,10 @@
 module "vpc" {
     source = "../terraform-aws-vpc"
     project_name=var.project_name
-    environment = var.environment
     comman_tags=var.comman_tags
+    public_subnet_cidrs = var.public_cidrs
+    private_subnet_cidrs = var.private_cidrs
+    database_subnet_cidrs=var.database_cidrs
    
   
 }
